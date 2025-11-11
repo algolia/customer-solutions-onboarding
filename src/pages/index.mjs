f6020c5888b6defde9396e2e8f25c9ba36de5c72
elavrienko <118895768+elavrienko@users.noreply.github.com>
2025-11-11 10:17:57 +0000

    Delete src directory

--- src/pages/index.mjs
+++
@@ -1,6 +0,0 @@
-import * as search from "./search.mjs";
-import * as home from "./home.mjs";
-import * as category from "./category.mjs";
-import * as movie from "./movie.mjs";
-
-export default { search, home, category, movie };
