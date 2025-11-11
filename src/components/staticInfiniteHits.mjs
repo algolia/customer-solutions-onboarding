51d8e55b83405be13d6d8c479caf9c5f54ea1d5e
elavrienko <118895768+elavrienko@users.noreply.github.com>
2025-11-11 10:15:43 +0000

    Upload code sandbox files

---
+++ src/components/staticInfiniteHits.mjs
@@ -0,0 +1,271 @@
+export default `
+<div class="ais-InfiniteHits">
+<ol class="ais-InfiniteHits-list">
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+  <li class="ais-InfiniteHits-item">
+    <div>
+      <a href="/movie/1234"
+        ><img
+          src="https://placehold.co/500x750?text=poster"
+          width="100%"
+        />
+        <h2>Movie Title</h2>
+        <p>9.99 €</p>
+        <button class="addToCard">Add to watch list</button></a
+      >
+    </div>
+  </li>
+</ol>
+<button class="ais-InfiniteHits-loadMore">
+  Show more results
+</button>
+</div>
+
+
+`;
